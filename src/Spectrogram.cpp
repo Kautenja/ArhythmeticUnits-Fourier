14c0dda2b9d25f492d53210dd8744620a77a1a88
Christian Kauten <kautencreations@gmail.com>
2025-02-14 00:06:07 -0500

    update

--- src/Spectrogram.cpp
+++ src/Spectrogram.cpp
@@ -288,7 +288,7 @@ struct Spectrogram : rack::Module {
         // If smoothing time is 0 or lower, alpha is always 0.
         if (smoothing_time <= 0.f) return 0.f;
         // Determine the hop-rate, i.e., the refresh rate of the DFT.
-        const float hop_time = N_FFT / 2;  // params[PARAM_HOP_LENGTH].getValue();
+        const float hop_time = (N_FFT / 2) / sample_rate;  // params[PARAM_HOP_LENGTH].getValue();
         // Calculate alpha relative to the hop-rate to keep time normalized.
         return expf(-10.f * hop_time / smoothing_time);
     }
